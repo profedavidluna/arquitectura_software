41a61a8a737f1b2be2c5680eb47658dc9f2ed05f
David Luna <davidrlunag@gmail.com>
2025-03-20 21:18:30 -0600

    Creating Dats

---
+++ soa/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Product (id,nombre,email, password) VALUES (1, 'Producto1', 'david@gmail.com','123456');
+INSERT INTO Product (id,nombre,email, password) VALUES (2, 'Producto2', 'david2@gmail.com','123456');
---
+++ soa/payment-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Pago (id,pedidoId, monto) VALUES (1, 1,3456);
+INSERT INTO Pago (id,pedidoId, monto) VALUES (2, 2,1221);
